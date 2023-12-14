7b6c02f76b1d2fc808a93a1612610678a74ee031
Raju Kancharla <119564575+rajukancharla-klubstack@users.noreply.github.com>
2023-12-15 00:59:31 +0530

    Integrated Likes with Exisitng Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
